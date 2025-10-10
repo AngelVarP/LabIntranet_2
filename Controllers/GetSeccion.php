13bd17c9acc3158993a24dec5319064d9f9cabc2
DinZ-24 <dino.suazo.z@uni.pe>
2025-10-10 13:04:17 -0500

    Borrando lo que no sirve

--- Controllers/GetSeccion.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../models/Seccion.php';
-
-header('Content-Type: application/json');
-
-$secciones = Seccion::obtenerSecciones();
-
-echo json_encode($secciones);
--- backend/config/db.php
+++ backend/config/db.php
@@ -3,7 +3,7 @@
 $DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = '';            // XAMPP por defecto
-$DB_NAME = 'labintranet3'; // la BD que creaste
+$DB_NAME = 'labintranet'; // la BD que creaste
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_errno) {
